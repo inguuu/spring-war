455045a45ad9ecd4b49b9dc89d54291ae7c022e9
ingue <dlsrb8949@gmail.com>
2019-07-29 14:54:56 +0900

    commit project

---
+++ study/src/test/java/com/example/study/StudyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
